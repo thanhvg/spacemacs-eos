5d943572ef9dfc316ecaff6b819085f560cb74ee
Thanh Vuong <thanhvg@gmail.com>
2020-03-21 13:29:27 -0600

    update hnreader bindings

--- packages.el
+++ packages.el
@@ -102,6 +102,8 @@ Each entry is either:
         "yhm" #'hnreader-more
         "yhs" #'hnreader-show
         "yhc" #'hnreader-comment
+        "yhr" #'hnreader-read-item
+        "yhi" #'hnreader-org-insert-hn-link
         "yhb" #'hnreader-back
         "yha" #'hnreader-ask))))
 
